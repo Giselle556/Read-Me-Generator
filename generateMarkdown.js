226182394d726333655674916f6e0533212755fc
Giselle556 <giselleborbolla@yahoo.com>
2021-09-15 23:49:30 -0700

    trying a new format for read me

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,13 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-
-
-
-
-
-
-
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+
+function renderLicenseBadge(license) {
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -20,10 +31,6 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-  ${data.description.join("\n")}
-
-  ${data.technology}
 
 `;
 }
